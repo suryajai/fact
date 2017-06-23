dff2c95b99db691bd763d33146e95895bf507eff
suryajai <arasidpharm@gmail.com>
2017-06-23 10:20:26 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class factorial {
+	public static void main(string[] args){
+		int a = 5;
+		int result = 1;
+		for (int i=0;i<=n;i++){
+			result=result*i;
+			System.out.println("the factorial of 7 is"+result);
+		}
+	}
+
+}
